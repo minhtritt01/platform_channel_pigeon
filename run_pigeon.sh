0e7a1226d8d2f13f9b079554996dfcea4417e2f8
Phan Tri <tri_phan@onskyhealth.com>
2024-07-18 15:41:28 +0700

    change name file generated

--- run_pigeon.sh
+++ run_pigeon.sh
@@ -2,7 +2,7 @@ dart run pigeon \
   --input pigeons/volume.dart \
   --dart_out lib/volume.dart \
   --objc_header_out ios/Runner/Classes/volume.h \
-  --swift_out ios/Runner/Classes/volume.swift \
+  --swift_out ios/Runner/Classes/volume.g.swift \
   --objc_prefix FLUTTER \
   --kotlin_out android/app/src/main/kotlin/com/example/volumelevel/Volume.g.kt \
   --kotlin_package "com.example.volumelevel"
