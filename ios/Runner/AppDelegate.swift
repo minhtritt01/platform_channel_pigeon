564087b698f39fb5d9a62b8603acbd3752ee447b
Phan Tri <tri_phan@onskyhealth.com>
2024-07-27 14:12:19 +0700

    use platform channel with pigeon

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,29 @@
 import Flutter
 import UIKit
-
+import AVFoundation
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, VolumeApi {
+    func getVolumeDevice() throws -> Volume {
+        let audioSession = AVAudioSession.sharedInstance()
+        let vol=audioSession.outputVolume
+        do {
+            try audioSession.setActive(true)
+        } catch {
+            print("Error Setting Up Audio Session")
+        }
+        print("volume \(type(of: vol))")
+        var result = Volume.init()
+        result.value = Double(vol)
+        return result
+    }
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      let controller = window?.rootViewController as! FlutterViewController
+      VolumeApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
